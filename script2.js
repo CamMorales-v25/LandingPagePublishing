3d5207cc9c21aa9975b62de65a16f781985fca20
CamMorales-v25 <cafemofa@gmail.com>
2023-11-23 03:19:37 -0500

    Se cambia

--- script2.js
+++ script2.js
@@ -1,21 +1,21 @@
 window.onload = function() {
-    let signUp= document.getElementById("signUp");
-    let signIn= document.getElementById("sigIn");
-    let nameInput= document.getElementById("nameInput");
-    let title= document.getElementById("title");
-
-    signIn.onclick= function(){
-        nameInput.style.maxHeight="0";
-        title.innerHTML="Login";
+    let signUp = document.getElementById("signUp");
+    let signIn = document.getElementById("sigIn");
+    let nameInput = document.getElementById("nameInput");
+    let title = document.getElementById("title");
+  
+    signIn.onclick = function() {
+        nameInput.style.maxHeight = "0";
+        title.innerHTML = "Cuenta Usuario ";
         signUp.classList.add("disable");
         signIn.classList.remove("disable");
     }
-
-    signUp.onclick= function(){
-        nameInput.style.maxHeight="60px";
-        title.innerHTML = "Registro";
+  
+    signUp.onclick = function() {
+        nameInput.style.maxHeight = "60px";
+        title.innerHTML = "Cuenta ONG";
         signUp.classList.remove("disable");
         signIn.classList.add("disable");
-        
     }
-}
+  }
+  
